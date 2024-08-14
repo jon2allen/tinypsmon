01001880ff798543f60f003d89751c5b04b06f25
jon allen <admin@jibsheet.net>
2024-08-13 22:37:25 -0400

    Added gzip support
    TODO - make gzip optional

--- include/logger.h
+++ include/logger.h
@@ -5,6 +5,7 @@
 #include <sstream>
 #include <stdexcept>
 #include <string>
+#include <zlib.h>
 
 class Logger {
   std::string filename;    // the name of the file to write to
@@ -54,6 +55,51 @@ private:
     return oss.str();
   }
 
+  void gzipFile(const std::string &filename) {
+    std::string gzFilename = filename + ".gz";
+    gzFile gzFile = gzopen(gzFilename.c_str(), "wb");
+    if (!gzFile) {
+      throw std::runtime_error("Failed to open gzip file " + gzFilename);
+    }
+
+    std::ifstream inFile(filename, std::ios::binary);
+    if (!inFile) {
+      gzclose(gzFile);
+      throw std::runtime_error("Failed to open file " + filename);
+    }
+
+    char buffer[1024];
+    while (inFile.read(buffer, sizeof(buffer))) {
+      gzwrite(gzFile, buffer, inFile.gcount());
+    }
+    gzwrite(gzFile, buffer, inFile.gcount()); // Write any remaining data
+
+    inFile.close();
+    gzclose(gzFile);
+
+    // Remove the original file after gzipping
+    std::filesystem::remove(filename);
+  }
+
+  // Function to delete files older than a specified number of days
+
+  void deleteOldFiles(const std::string &directory, int daysOld) {
+    auto now = std::chrono::system_clock::now();
+    for (const auto &entry : std::filesystem::directory_iterator(directory)) {
+      auto fileTime = std::filesystem::last_write_time(entry);
+      auto sctp =
+          std::chrono::time_point_cast<std::chrono::system_clock::duration>(
+              fileTime - std::filesystem::file_time_type::clock::now() +
+              std::chrono::system_clock::now());
+      auto fileAge =
+          std::chrono::duration_cast<std::chrono::hours>(now - sctp).count() /
+          24;
+      if (fileAge > daysOld) {
+        std::filesystem::remove(entry);
+      }
+    }
+  }
+
   // Helper method to handle date change
   void handleDateChange() {
     // Get the current date
@@ -68,6 +114,9 @@ private:
       std::string newFilename = filename + "." + lastLogDate;
       std::filesystem::rename(filename, newFilename);
 
+      // Gzip the renamed file
+      gzipFile(newFilename);
+
       // Open a new file for logging
       file.open(filename, std::ios::out | std::ios::app);
       if (!file) {
@@ -78,7 +127,6 @@ private:
       lastLogDate = currentDate;
     }
   }
-
   // Helper method to log a message with a timestamp
   void logMessageWithTimestamp(const std::string &message) {
     // Get the current time
